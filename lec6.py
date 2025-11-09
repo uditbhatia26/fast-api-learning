eb5a3c1b8e09df81ef5eb629cb978ca90f36309f
Udit26085 <bhatiaudit55@gmail.com>
2025-11-09 20:59:54 +0530

    Fix gender field

--- lec6.py
+++ lec6.py
@@ -33,7 +33,7 @@ class PatientUpdate(BaseModel):
     name: Annotated[Optional[str], Field(default=None)]
     city: Annotated[Optional[str], Field(default=None)]
     age: Annotated[Optional[int], Field(default=None, gt=0)]
-    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    gender: Annotated[Optional[Literal["Male", "Female"]], Field(default=None)]
     height: Annotated[Optional[float], Field(default=None, gt=0)]
     weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
